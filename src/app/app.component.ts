2eaa85285e4c0506cc5302f0dea4ba91645987c3
Mark Burggraf <markb@mantisbible.com>
2021-06-23 14:44:09 -0700

    sign in, sign out from menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
+import { User } from '@supabase/supabase-js';
+import { SupabaseService } from './services/supabase.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public user: User = null;
+
   public appPages = [
 
-    { title: 'Home', url: 'home', icon: 'home' },
-    { title: 'Login', url: 'login', icon: 'log-in' },
+    { title: 'Home', url: 'home', icon: 'home' }
   ];
 
-  constructor() {}
+  constructor(private supabaseService: SupabaseService) {
+    this.supabaseService.user.subscribe((user: User) => {
+      this.user = user;
+    });  
+  }
+
+  async signOut() {
+    const { error } = await this.supabaseService.signOut();
+    console.log('signOut', error);
+  }
+
 }
